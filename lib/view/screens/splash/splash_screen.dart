d450cdfc092c3f26667fff1a8b7a7b0dc9b74a5d
omer taj <omertaj39@gmail.com>
2021-05-21 14:16:54 +0400

    Initial commit

---
+++ lib/view/screens/splash/splash_screen.dart
@@ -0,0 +1,86 @@
+import 'dart:async';
+
+import 'package:emarket_delivery_boy/utill/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:emarket_delivery_boy/localization/language_constrants.dart';
+import 'package:emarket_delivery_boy/provider/auth_provider.dart';
+import 'package:emarket_delivery_boy/provider/splash_provider.dart';
+import 'package:emarket_delivery_boy/utill/images.dart';
+import 'package:emarket_delivery_boy/view/screens/dashboard/dashboard_screen.dart';
+import 'package:emarket_delivery_boy/view/screens/language/choose_language_screen.dart';
+import 'package:provider/provider.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    _route();
+  }
+
+  void _route() {
+    Provider.of<SplashProvider>(context, listen: false).initSharedData();
+    Provider.of<SplashProvider>(context, listen: false).initConfig(context).then((bool isSuccess) {
+      if (isSuccess) {
+        Timer(Duration(seconds: 1), () async {
+          if (Provider.of<AuthProvider>(context, listen: false).isLoggedIn()) {
+            Provider.of<AuthProvider>(context, listen: false).updateToken();
+            _checkPermission(DashboardScreen());
+          } else {
+            _checkPermission(ChooseLanguageScreen());
+          }
+
+        });
+      }
+    });
+  }
+
+  void _checkPermission(Widget navigateTo) async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if(permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    if(permission == LocationPermission.denied) {
+      showDialog(context: context, barrierDismissible: false, builder: (context) => AlertDialog(
+        title: Text(getTranslated('alert', context)),
+        content: Text(getTranslated('allow_for_all_time', context)),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        actions: [ElevatedButton(
+          onPressed: () async {
+            Navigator.pop(context);
+            await Geolocator.requestPermission();
+            _checkPermission(navigateTo);
+          },
+          child: Text(getTranslated('ok', context)),
+        )],
+      ));
+    }else if(permission == LocationPermission.deniedForever) {
+      await Geolocator.openLocationSettings();
+    }else {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => navigateTo));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(Images.logo, width: 200),
+            Text('eMarket', style: rubikBold.copyWith(fontSize: 30, color: Theme.of(context).primaryColor)),
+          ],
+        ),
+      ),
+    );
+  }
+}
